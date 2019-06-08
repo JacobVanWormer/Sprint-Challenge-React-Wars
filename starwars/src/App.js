8f9d75f1ab03e4dc10d0e40d3536a795f00f571c
Jacob VanWormer <sharpiejacob@gmail.com>
2019-06-07 17:06:32 -0700

    added file for Characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import StarwarsCharacters from './Characters';
+
 
 class App extends Component {
   constructor() {
@@ -29,13 +31,17 @@ class App extends Component {
       });
   };
 
+
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <h1 className="Header">React Wars</h1>
+        <StarwarsCharacters />
       </div>
     );
   }
 }
+
+
 
 export default App;
